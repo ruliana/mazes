667b2532398a189894787342b372867c939d3a55
Ronie Uliana <ronie.uliana@gmail.com>
2019-09-17 22:54:57 -0400

    Using a queue instead of lists
    A mutable queue, currently. I'd like to use immutabe structs on a future
    implementation.

--- maze.scm
+++ maze.scm
@@ -47,40 +47,43 @@
 
 (define-class <distance> ()
   (root init-word: #:root getter: root)
-  (cells getter: cells))
+  (content getter: content))
 
 (define-method (initialize (self <distance>) initargs)
   (let-keywords initargs #f
                 ([root #f]
-                 [cells (make-hash-table eq?)])
-    (slot-set! self 'cells cells)
+                 [content (make-hash-table eq?)])
+    (slot-set! self 'content content)
     (slot-set! self 'root root)
-    (hash-table-set! cells root 0)))
+    (hash-table-set! content root 0)))
 
 (define-method (ref (self <distance>) (cell <cell>))
-  (hash-table-ref/default (cells self) cell #f))
+  (hash-table-ref/default (content self) cell #f))
 
 (define-method (set (self <distance>) (cell <cell>) (distance <number>))
-  (hash-table-set! (cells self) cell distance))
+  (hash-table-set! (content self) cell distance))
+
+(define-method (longest (self <distance>))
+  (apply max (hash-table-values (content self))))
 
 (define-method (distances (root <cell>))
   (define distance (make <distance> root: root))
+  (define frontier (enq! (make-q) root))
   (define (head-visited? lst) (ref distance (first lst)))
   (define (visit cell)
-    (visit-neighbors (links cell) '() (ref distance cell)))
-  (define (visit-neighbors neighs new-frontier dist)
+    (visit-neighbors (links cell) (ref distance cell)))
+  (define (visit-neighbors neighs dist)
     (match neighs
-      [() new-frontier]
+      [() #f]
       [(? head-visited? (head tail ...))
-       (visit-neighbors tail new-frontier dist)]
+       (visit-neighbors tail dist)]
       [(head tail ...)
        (set distance head (1+ dist))
-       (visit-neighbors tail (cons head new-frontier) dist)]))
-  (let loop ([frontier (list root)])
-    (match frontier
-      [() distance]
-      [(head tail ...)
-       (loop (append tail (visit head)))])))
+       (enq! frontier head)
+       (visit-neighbors tail dist)]))
+  (do ()
+      [(q-empty? frontier) distance]
+      (visit (deq! frontier))))
 
 ;; Display maze
 (define (display-maze-ascii algorithm rows cols)
@@ -88,22 +91,13 @@
 
 (define (display-maze-graph file-name algorithm r c)
   (let* ([maze (algorithm (make <grid> rows: r cols: c))]
-         [distance (distances (ref maze 0 0))])
+         [distance (distances (ref maze 0 0))]
+         [max-distance (* 0.5 (longest distance))])
     (->svg file-name
            (rows maze)
            (cols maze)
            (map ->bits (cells maze))
-           (map (λ (e) (- 1 (/ (or (ref distance e) 80) 80))) (cells maze)))
+           (map (λ (e) (- 1 (/ (or (ref distance e) max-distance) max-distance))) (cells maze)))
     (values maze distance)))
 
-(define (test)
-  (let ([maze (sidewinder! (make <grid> rows: 3 cols: 3))])
-     (list maze (distances (ref maze 0 0)))))
-
-(define-values (maze distance) (display-maze-graph "./labyrinth.svg" (Λ sidewinder! <> 0.9) 30 60))
-(let loop ([items (cells maze)])
-  (match items
-    [() #f]
-    [(head tail ...)
-     (format #t "~a ~a\n" (display head #f) (ref distance head))
-     (loop tail)]))
+(define-values (maze distance) (display-maze-graph "./labyrinth.svg" (Λ sidewinder! <> 0.9) 38 80))
